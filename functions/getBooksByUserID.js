6cf5f2e3749d037792953412d4a9d0aeefe62f62
fady-george <fady.george@blink22.com>
2022-09-20 16:16:53 +0200

    test error

--- functions/getBooksByUserID.js
+++ functions/getBooksByUserID.js
@@ -1,21 +1,7 @@
 const { BSON } = require("realm-web");
 
 exports = async function getBooks(req, _) {
-  const { query } = req
-  const mongoDB = context.services.get("mongodb-atlas");
-  const users = mongoDB.db("testingDB").collection("users");
-  const books = mongoDB.db("testingDB").collection("books");
 
-  if (query.id) {
-    let user = await users.findOne({ _id: new BSON.ObjectId(query.id) });
-    let listOFBookIds = Object.values(user.booksRead).map((bookIDAsString) => BSON.ObjectId(bookIDAsString))
-    let booksOfUser = await books.find({
-      _id: {
-        $in: listOFBookIds
-      }
-    })
-    return booksOfUser;
-  }
 
   return { "error": "please enter id in your query" };
 };
