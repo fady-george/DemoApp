787092d06a143ff998b2805da2617ceadb6a1d6c
fady-george <fady.george@blink22.com>
2022-09-20 17:46:09 +0200

    add files

---
+++ functions/getBooksByUserID.js
@@ -0,0 +1,19 @@
+exports = async function getBooksByUserID(req, _) {
+  const { query } = req
+  const mongoDB = context.services.get("mongodb-atlas");
+  const users = mongoDB.db("testingDB").collection("users");
+  const books = mongoDB.db("testingDB").collection("books");
+
+  if (query.id) {
+    let user = await users.findOne({ _id: new BSON.ObjectId(query.id) });
+    let listOFBookIds = Object.values(user.booksRead).map((bookIDAsString) => BSON.ObjectId(bookIDAsString))
+    let booksOfUser = await books.find({
+      _id: {
+        $in: listOFBookIds
+      }
+    })
+    return booksOfUser;
+  }
+
+  return { "error": "please enter id in your query" };
+};
